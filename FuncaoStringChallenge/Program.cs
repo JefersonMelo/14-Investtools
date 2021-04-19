7e9754f86ec7c6bb055c0f66f31f41faa8d5816d
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-19 15:19:31 -0300

    commit final

--- FuncaoStringChallenge/Program.cs
+++ FuncaoStringChallenge/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Globalization;
 
 namespace FuncaoStringChallenge
 {
     class Program
     {
+        public static string StringChallenge(string str)
+        {
+            // code goes here  
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(StringChallenge(Console.ReadLine()));
         }
     }
 }
