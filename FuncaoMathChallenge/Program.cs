d19604dd632b4ebf5d6e9379172da1a09cdf6ffd
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-19 15:25:31 -0300

    commit final

--- FuncaoMathChallenge/Program.cs
+++ FuncaoMathChallenge/Program.cs
@@ -4,9 +4,27 @@ namespace FuncaoMathChallenge
 {
     class Program
     {
+        public static bool MathChallenge(int num)
+        {
+            // code goes here  
+            int cont = 0;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0) cont++;
+            }
+            if (cont == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // keep this function call here
+            Console.WriteLine(MathChallenge(Console.ReadLine()));
         }
     }
 }
